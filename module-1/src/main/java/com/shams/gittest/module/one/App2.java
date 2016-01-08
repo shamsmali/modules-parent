7b7efedb53e72845ce5701b9df894873162e888f
Shams <shams.mali@live.com>
2016-01-08 15:17:29 -0800

    Added

---
+++ module-1/src/main/java/com/shams/gittest/module/one/App2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shams.gittest.module.one;
+
+/**
+ * @author shams
+ *
+ */
+public class App2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
