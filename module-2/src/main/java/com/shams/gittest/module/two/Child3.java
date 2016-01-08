c7531a623e106c1bfaeaf8df6eefbcb4220eb4bd
Shams <shams.mali@live.com>
2016-01-08 15:32:16 -0800

    module 2 pushes child3

---
+++ module-2/src/main/java/com/shams/gittest/module/two/Child3.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shams.gittest.module.two;
+
+/**
+ * @author shams
+ *
+ */
+public class Child3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
