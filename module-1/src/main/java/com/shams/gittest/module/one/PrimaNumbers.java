eb86588b1724e70badf88cd074ba2d45593e9e72
Shams <shams.mali@live.com>
2016-01-08 15:40:34 -0800

    stashed changes took backl

---
+++ module-1/src/main/java/com/shams/gittest/module/one/PrimaNumbers.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.shams.gittest.module.one;
+
+/**
+ * @author shams
+ *
+ */
+public class PrimaNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("dsf sdfsd");
+	}
+
+}
---
+++ module-2/src/main/java/com/shams/gittest/module/two/SquareRoot.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.shams.gittest.module.two;
+
+/**
+ * @author shams
+ *
+ */
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		System.out.println("dsf sdfsd");
+	}
+}
