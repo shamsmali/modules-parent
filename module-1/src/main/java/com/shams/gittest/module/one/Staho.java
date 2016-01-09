9c3adadf45682d52490cd10fb12a652f086a137b
Shams <shams.mali@live.com>
2016-01-08 16:03:28 -0800

    kj

--- module-1/src/main/java/com/shams/gittest/module/one/Staho.java
+++ module-1/src/main/java/com/shams/gittest/module/one/Staho.java
@@ -3,8 +3,7 @@ package com.shams.gittest.module.one;
 public class Staho {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("staho ksdh");
 	}
 
 }
